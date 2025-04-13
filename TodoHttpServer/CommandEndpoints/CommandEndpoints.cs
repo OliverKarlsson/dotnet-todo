3fa7a265ea4465f00a8f03fa5d77534fc46b5c83
Oliver N.Karlsson <o.a.n.karlsson@gmail.com>
2025-04-13 19:51:14 +0200

    add hardcoded examples of todo's create, complete and undo complete

--- TodoHttpServer/CommandEndpoints/CommandEndpoints.cs
+++ TodoHttpServer/CommandEndpoints/CommandEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using TodoHttpServer.CommandEndpoints.Model;
 
 namespace TodoHttpServer.CommandEndpoints
 {
@@ -7,6 +8,21 @@ namespace TodoHttpServer.CommandEndpoints
         public static void MapCommandEndpoints(this IEndpointRouteBuilder app)
         {
             app.MapGet("/commands/example", () => "This is a command endpoint");
+
+            app.MapPost("/todos/create", async (CreateTodo todoItem) =>
+            {
+                return Results.Created($"/todos/6", todoItem);
+            });
+
+            app.MapPost("/todos/{TodoId}/complete", async (int TodoId) =>
+            {
+                return Results.Ok($"Todo with ID {TodoId} marked as complete.");
+            });
+
+            app.MapPost("/todos/{TodoId}/undo-complete", async (int TodoId) =>
+            {
+                return Results.Ok($"Completion undone for Todo with ID {TodoId}.");
+            });
         }
     }
 }
--- TodoHttpServer/obj/Debug/net9.0/TodoHttpServer.AssemblyInfo.cs
+++ TodoHttpServer/obj/Debug/net9.0/TodoHttpServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoHttpServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d800fe6dec3cacddd7c83a183ad7c29d965c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a806e64ee200e15afb1e62b51865dfc5fb44328b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoHttpServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoHttpServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
