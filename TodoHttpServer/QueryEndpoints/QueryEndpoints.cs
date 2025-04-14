b7c9b36f03f238d4a671a9348b8d45085eff0294
Oliver N.Karlsson <o.a.n.karlsson@gmail.com>
2025-04-14 21:54:59 +0200

    add fetch from the data repository when fetching todos

--- TodoHttpServer/QueryEndpoints/QueryEndpoints.cs
+++ TodoHttpServer/QueryEndpoints/QueryEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using TodoHttpServer.QueryEndpoints.Model;
 
 namespace TodoHttpServer.QueryEndpoints
@@ -7,26 +8,26 @@ namespace TodoHttpServer.QueryEndpoints
     {
         public static void MapQueryEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/todos", async () =>
+            app.MapGet("/todos", async ([FromServices] TodoRepository repository) =>
             {
-                var todos = new TodoItemModel[]
+                var todos = await repository.GetAllTodosAsync();
+                var todosWithCompleted = todos.Select(todo => new TodoItemModel
                 {
-                    new TodoItemModel { Id = 1, Name = "Buy groceries", CreationDate = "2025-01-01", Completed = true },
-                    new TodoItemModel { Id = 2, Name = "Clean desk", CreationDate = "2025-01-02", Completed = true },
-                    new TodoItemModel { Id = 3, Name = "Water plants", CreationDate = "2025-01-03", Completed = false },
-                    new TodoItemModel { Id = 4, Name = "Finish report", CreationDate = "2025-01-04", Completed = false },
-                    new TodoItemModel { Id = 5, Name = "Plan vacation", CreationDate = "2025-01-05", Completed = false }
-                };
-                return Results.Ok(todos);
+                    Id = todo.Id,
+                    Name = todo.Name,
+                    CreationDate = todo.CreationDate,
+                    Completed = false
+                });
+                return Results.Ok(todosWithCompleted);
             });
 
             app.MapGet("/todos/active", async () =>
             {
                 var todos = new TodoItemModel[]
                 {
-                    new TodoItemModel { Id = 3, Name = "Water plants", CreationDate = "2025-01-03", Completed = false },
-                    new TodoItemModel { Id = 4, Name = "Finish report", CreationDate = "2025-01-04", Completed = false },
-                    new TodoItemModel { Id = 5, Name = "Plan vacation", CreationDate = "2025-01-05", Completed = false }
+                       new TodoItemModel { Id = 3, Name = "Water plants", CreationDate = "2025-01-03", Completed = false },
+                       new TodoItemModel { Id = 4, Name = "Finish report", CreationDate = "2025-01-04", Completed = false },
+                       new TodoItemModel { Id = 5, Name = "Plan vacation", CreationDate = "2025-01-05", Completed = false }
                 };
                 return Results.Ok(todos);
             });
@@ -35,12 +36,11 @@ namespace TodoHttpServer.QueryEndpoints
             {
                 var todos = new TodoItemModel[]
                 {
-                    new TodoItemModel { Id = 1, Name = "Buy groceries", CreationDate = "2025-01-01", Completed = true },
-                    new TodoItemModel { Id = 2, Name = "Clean desk", CreationDate = "2025-01-02", Completed = true },
+                       new TodoItemModel { Id = 1, Name = "Buy groceries", CreationDate = "2025-01-01", Completed = true },
+                       new TodoItemModel { Id = 2, Name = "Clean desk", CreationDate = "2025-01-02", Completed = true },
                 };
                 return Results.Ok(todos);
             });
-
         }
     }
 
