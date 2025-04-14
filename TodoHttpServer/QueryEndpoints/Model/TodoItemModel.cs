1c50b42b9c6ff870f42a7da4d457cbd8ba109e19
Oliver N.Karlsson <o.a.n.karlsson@gmail.com>
2025-04-14 21:53:00 +0200

    make the model properties non-nullable

--- TodoHttpServer/QueryEndpoints/Model/TodoItemModel.cs
+++ TodoHttpServer/QueryEndpoints/Model/TodoItemModel.cs
@@ -3,9 +3,9 @@ namespace TodoHttpServer.QueryEndpoints.Model
 {
     public class TodoItemModel
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string CreationDate { get; set; }
-        public bool Completed { get; set; }
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string CreationDate { get; set; }
+        public required bool Completed { get; set; }
     }
 }
