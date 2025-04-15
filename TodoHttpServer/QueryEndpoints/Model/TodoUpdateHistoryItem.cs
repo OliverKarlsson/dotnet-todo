d6f096d0a94344ba2c7c6bbc10d78068ff1be3f0
Oliver N.Karlsson <o.a.n.karlsson@gmail.com>
2025-04-15 22:16:49 +0200

    add endpoint for todo updates

---
+++ TodoHttpServer/QueryEndpoints/Model/TodoUpdateHistoryItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TodoHttpServer.QueryEndpoints.Model
+{
+    public class TodoUpdateHistoryItem
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Status { get; set; }
+        public required string EventTimeStamp { get; set; }
+    }
+}
